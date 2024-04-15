701607ff9419524c5f7b52f8c1eab374c2606cca
Felipe Portari <124217908+felipeportari@users.noreply.github.com>
2024-04-15 18:52:14 -0300

    Alterando repositorys

--- src/main/java/com/example/demo1/repository/CategoriaRepository.java
+++ src/main/java/com/example/demo1/repository/CategoriaRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo1.models.Categoria;
 
+import java.util.List;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    
+    List<Categoria> findByNomeStartingWith(String nome);
+
+    @Query("SELECT c FROM Categoria c LEFT JOIN FETCH c.produtos WHERE c.id = :id")
+    Categoria findByIdAndFetchProdutosEagerly(@Param("id") Long id);
 }
--- src/main/java/com/example/demo1/repository/ProdutoRepository.java
+++ src/main/java/com/example/demo1/repository/ProdutoRepository.java
@@ -1,8 +1,17 @@
 package com.example.demo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo1.models.Produto;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    List<Produto> findByPrecoGreaterThan(Double valor);
+
+    List<Produto> findByPrecoLessThanEqual(Double valor);
+
+    List<Produto> findByNomeStartingWith(String nome);
 }
